26bfed381fb2878ae3ffc6721de5774e4850117b
Mohammad  Asghar <63851590+asghar512@users.noreply.github.com>
2021-02-07 22:31:05 +0500

    Build CovidTracker
    create CovidTracker using React.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Details from './Details';
+
+
+function App() {
+  return (
+    <div >
+      <Details/>
+    </div>
+  );
+}
+
+export default App;
